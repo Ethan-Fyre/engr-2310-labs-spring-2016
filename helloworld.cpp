e54b28ed2efe1f67cdf72504e92cedbdd6b6f550
Ethan Sayles <jasayles@anderson.edu>
2018-02-03 00:17:40 -0500

    C++ code for lab 1.

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "HH   HH  EEEEEEE  LL        LL        OOOOO  " << endl;
+    cout << "HH   HH  EE       LL        LL       OO   OO " << endl;
+    cout << "HHHHHHH  EEEEE    LL        LL      OO     OO" << endl;
+    cout << "HH   HH  EE       LL        LL       OO   OO " << endl;
+    cout << "HH   HH  EEEEEEE  LLLLLLL   LLLLLLL   OOOOO  " << endl;
+    return 0;
+}
